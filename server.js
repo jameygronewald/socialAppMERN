a37423d2dc085b5628c0b146f198f49ea1c84fce
Jamey Gronewald <jrgronewald@gmail.com>
2020-10-29 15:06:51 -0700

    added route files for rest api and defined user schema; user post request returning request body

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Database connection
+connectDB();
+
+// Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/api/running', (req, res) => res.json({ success: true }));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
