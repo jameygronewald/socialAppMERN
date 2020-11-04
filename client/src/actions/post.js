dd738363e4c5dc5c8d6f071aeabb60d14474ff63
Jamey Gronewald <jrgronewald@gmail.com>
2020-11-04 14:49:52 -0800

    added post form for adding new posts; added redux flow for adding posts and deleting posts and attached action to necessary button event

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './constants';
+import { GET_POSTS, ADD_POST, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './constants';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -46,11 +46,60 @@ export const addLike = id => async dispatch => {
 export const removeLike = id => async dispatch => {
   try {
     const res = await axios.put(`/api/posts/unlike/${id}`);
-
     dispatch({
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = id => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert('Post removed', 'danger'));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(`/api/posts`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Post added!', 'success'));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../../actions/post';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   post: { _id, text, name, avatar, user, likes, comments, date },
   auth,
 }) => {
@@ -24,11 +25,19 @@ const PostItem = ({
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button onClick={e => addLike(_id)} type='button' className='btn btn-light'>
+        <button
+          onClick={e => addLike(_id)}
+          type='button'
+          className='btn btn-light'
+        >
           <i className='fas fa-thumbs-up'></i>{' '}
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button onClick={e => removeLike(_id)} type='button' className='btn btn-light'>
+        <button
+          onClick={e => removeLike(_id)}
+          type='button'
+          className='btn btn-light'
+        >
           <i className='fas fa-thumbs-down'></i>
         </button>
         <Link to={`/post/${_id}`} className='btn btn-primary'>
@@ -38,7 +47,11 @@ const PostItem = ({
           )}
         </Link>
         {!auth.loading && user === auth.user._id && (
-          <button type='button' className='btn btn-danger'>
+          <button
+            onClick={e => deletePost(_id)}
+            type='button'
+            className='btn btn-danger'
+          >
             <i className='fas fa-times'></i>
           </button>
         )}
@@ -50,12 +63,15 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
-//   addLike: PropTypes.func.isRequired,
-//   removeLike: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
