5230207a86bc36ee6fb2c849ddfa5fdc71bc9968
Jamey Gronewald <jrgronewald@gmail.com>
2020-11-06 13:28:56 -0800

    abstracted out routes to add a catchall not found page

---
+++ client/src/components/routing/Routes.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../profileForms/CreateProfile';
+import EditProfile from '../profileForms/EditProfile';
+import AddExperience from '../profileForms/AddExperience';
+import AddEducation from '../profileForms/AddEducation';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
+import NotFound from '../layout/NotFound';
+import PrivateRoute from '../routing/PrivateRoute';
+
+const Routes = () => {
+  return (
+    <section className='container'>
+      <Alert />
+      <Switch>
+        <Route exact path='/register' component={Register}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/profiles' component={Profiles}></Route>
+        <Route exact path='/profile/:id' component={Profile}></Route>
+        <PrivateRoute
+          exact
+          path='/dashboard'
+          component={Dashboard}
+        ></PrivateRoute>
+        <PrivateRoute
+          exact
+          path='/create-profile'
+          component={CreateProfile}
+        ></PrivateRoute>
+        <PrivateRoute
+          exact
+          path='/edit-profile'
+          component={EditProfile}
+        ></PrivateRoute>
+        <PrivateRoute
+          exact
+          path='/add-experience'
+          component={AddExperience}
+        ></PrivateRoute>
+        <PrivateRoute
+          exact
+          path='/add-education'
+          component={AddEducation}
+        ></PrivateRoute>
+        <PrivateRoute exact path='/posts' component={Posts}></PrivateRoute>
+        <PrivateRoute exact path='/posts/:id' component={Post}></PrivateRoute>
+        <Route component={NotFound} />
+      </Switch>
+    </section>
+  );
+};
+
+export default Routes;
